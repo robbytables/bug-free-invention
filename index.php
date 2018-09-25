e4fd9e81449b8c2d1483597bceb194d256a92936
conductiveio <grodin.robby@gmail.com>
2018-09-24 21:28:28 -0400

    Adds hw 1

--- index.php
+++ index.php
@@ -35,11 +35,14 @@
 
 
 <h2>Unit 1</h2>
-<p><a target="_blank"  href="unit-1-variables/instructor-resources/01-welcome/01-welcome-slides.md.html">unit-1-variables/instructor-resources/01-welcome/01-welcome-slides.md</a></p>
-<p><a target="_blank"  href="unit-1-variables/instructor-resources/02-variables/02-variables-slides.md.html">unit-1-variables/instructor-resources/02-variables/02-variables-slides.md</a></p>
-<p><a target="_blank"  href="unit-1-variables/instructor-resources/03-local-python/03-local-python-slides.md.html">unit-1-variables/instructor-resources/03-local-python/03-local-python-slides.md</a></p>
-<p><a target="_blank"  href="unit-1-variables/instructor-resources/03-local-python/python3-installation-directions.md.html">unit-1-variables/instructor-resources/03-local-python/python3-installation-directions.md</a></p>
+<p><a target="_blank"  href="unit-1-variables/instructor-resources/01-welcome/01-welcome-slides.md.html">Lesson 01: Welcome!</a></p>
+<p><a target="_blank"  href="unit-1-variables/instructor-resources/02-variables/02-variables-slides.md.html">Lesson 02: Variables</a></p>
+<p><a target="_blank"  href="unit-1-variables/instructor-resources/03-local-python/03-local-python-slides.md.html">Lesson 03: Local Python</a></p>
+<p><a target="_blank"  href="unit-1-variables/instructor-resources/03-local-python/python3-installation-directions.md.html">Python 3 Installation Instructions</a></p>
 
+
+<h3> Unit 1 Homework: Due Wednesday, September 26 </h3>
+<p><a target="_black" href="unit-1-variables/instructor-resources/hw-10wk-class-1/README.md.html">Unit 1 Homework: Variables</a></p>
 
 </body>
 
